0a1e955373497b048ca8b055bfff3e9211cd4870
Witty Chang <wcc1213@gmail.com>
2017-06-02 11:46:01 -0700

    Add input forms and select boxes and add content to README.md.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
   $("form#userTrack").submit(function(event) {
-    event.preventDefault();
-    var learner = parseInt$("#learner").val();
-    var priority = parseInt$("#priority").val();
-    var employer = parseInt$("#employer").val();
-    var webby = parseInt$("#webby").val();
-    var gameplay = parseInt$("#gameplay").val();
+    var user1Input = $("input#user1").val();
+    var learner = parseInt($("#learner").val());
+    var priority = parseInt($("#priority").val());
+    var employer = parseInt($("#employer").val());
+    var webby = parseInt($("#webby").val());
+    var gameplay = parseInt($("#gameplay").val());
 
     var result = learner + priority + employer + webby + gameplay;
 
-    if (result >=5 && <= 9) {
-      $("cssDesign").fadeIn();
-    } else if (result > 9 && <= 12) {
-      $("javaAndroid").fadeIn();
-    } else if (result > 12 && <=15) {
-      $("phpDrupal").fadeIn();
-    } else {
-      alert('Please make a selection.')
+    $('.user1').text(user1Input);
+
+    if (learner === 0) {
+      alert('Please make a selection.');
+    } else if (priority === 0) {
+      alert('Please make a selection.');
+    } else if (employer === 0) {
+      alert('Please make a selection.');
+    } else if (webby === 0) {
+      alert('Please make a selection.');
+    } else if (gameplay === 0) {
+      alert('Please make a selection.');
+    } else if (result >=5 && result <=9) {
+      $('#conclusion1').show();
+    } else if (result > 9 && result <= 12) {
+      $('#conclusion2').show();
+    } else if (result > 12 && result <=15) {
+      $('#conclusion3').show();
     }
-  })
+
+    event.preventDefault();
+  });
 });
